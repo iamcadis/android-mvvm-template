ee076c1f2d973fe0d1fa27835d9d4d816af82735
iamcadis <hangoluancadis@gmail.com>
2021-03-11 12:07:32 +0700

    Add extra ui for loading & lost connection

--- app/src/main/java/com/apps/ui/MainActivity.kt
+++ app/src/main/java/com/apps/ui/MainActivity.kt
@@ -4,15 +4,13 @@ import android.os.Bundle
 import com.apps.databinding.ActivityMainBinding
 import com.apps.interfaces.OnLostConnection
 import com.apps.ui.base.BaseActivity
-import com.apps.ui.extra.LostConnectionSheet
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>({ ActivityMainBinding.inflate(it) }), OnLostConnection {
 
     override fun ActivityMainBinding.onCreate(savedInstanceState: Bundle?) {
-        val sheet = LostConnectionSheet.newInstance(null, this@MainActivity)
-        sheet.showNow(supportFragmentManager, sheet.javaClass.simpleName)
+
     }
 
     override fun onRetry(passingCode: String?) {
--- app/src/main/java/com/apps/utils/extensions/ViewExt.kt
+++ app/src/main/java/com/apps/utils/extensions/ViewExt.kt
@@ -1,17 +1,50 @@
 package com.apps.utils.extensions
 
 import android.view.View
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import coil.ImageLoader
+import coil.request.ImageRequest
+import com.apps.R
 
 fun View.visibleOrGone(isVisible: Boolean) {
-    this.visibility = if (isVisible) View.VISIBLE else View.GONE
+    visibility = if (isVisible) View.VISIBLE else View.GONE
 }
 
 fun View.visibleOrInvisible(isVisible: Boolean) {
-    this.visibility = if (isVisible) View.VISIBLE else View.INVISIBLE
+    visibility = if (isVisible) View.VISIBLE else View.INVISIBLE
+}
+
+fun ImageView.show(url: String?) {
+    val imageLoader = ImageLoader(this.context)
+    val request = ImageRequest.Builder(this.context)
+            .data(url)
+            .crossfade(true)
+            .error(R.drawable.svg_no_image)
+            .placeholder(R.drawable.svg_placeholder)
+            .target(
+                    onStart = { placeholder ->
+                        setBackgroundColor(ContextCompat.getColor(context, R.color.lighter_gray))
+                        scaleType = ImageView.ScaleType.CENTER_INSIDE
+                        setImageDrawable(placeholder)
+                    },
+                    onSuccess = { result ->
+                        setBackgroundColor(ContextCompat.getColor(context, R.color.lighter_gray))
+                        scaleType = ImageView.ScaleType.CENTER_CROP
+                        setImageDrawable(result)
+                    },
+                    onError = { error ->
+                        setBackgroundColor(ContextCompat.getColor(context, R.color.lighter_gray))
+                        scaleType = ImageView.ScaleType.CENTER_INSIDE
+                        setImageDrawable(error)
+                    }
+            )
+            .build()
+    imageLoader.enqueue(request)
 }
 
 fun RecyclerView.toLinear(
@@ -19,7 +52,7 @@ fun RecyclerView.toLinear(
         orientation: Int = RecyclerView.VERTICAL,
         isHasFixedSize: Boolean = true
 ) {
-    this.apply {
+    apply {
         setHasFixedSize(isHasFixedSize)
         layoutManager = LinearLayoutManager(context, orientation, false)
         itemAnimator = DefaultItemAnimator()
@@ -32,7 +65,7 @@ fun RecyclerView.toGrid(
         column: Int,
         isHasFixedSize: Boolean = true
 ) {
-    this.apply {
+    apply {
         setHasFixedSize(isHasFixedSize)
         layoutManager = GridLayoutManager(context, column)
         itemAnimator = DefaultItemAnimator()
